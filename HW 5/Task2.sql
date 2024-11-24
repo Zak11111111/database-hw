7f73ff64b907d1fdd323911a652e06ee14408906
Zak11111111 <zak.ghadialy@city.ac.uk>
2024-11-24 21:01:46 +0000

    Add files via upload

---
+++ HW 5/Task2.sql
@@ -0,0 +1,14 @@
+SELECT SUM(bill_total) AS Income
+FROM restBill;
+SELECT SUM(bill_total) AS 'Feb Income'
+FROM restBill
+WHERE bill_date BETWEEN 160201 AND 160229;
+SELECT AVG(bill_total) AS Average_Bill
+FROM restBill
+WHERE table_no = 2;
+SELECT MIN(no_of_seats) AS Min, MAX(no_of_seats) AS Max, AVG(no_of_seats) AS Avg
+FROM restRest_table
+WHERE room_name = 'Blue';
+SELECT COUNT(DISTINCT table_no) AS Table_Count
+FROM restBill
+WHERE waiter_no IN (004, 002);
